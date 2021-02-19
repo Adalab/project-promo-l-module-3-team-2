a8198d6ee961fcb4cc8030c43b5fde496ce624cc
TeresaMartinezFernandez <teresamf64@gmail.com>
2021-02-19 19:38:51 +0100

    Add component design

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,81 +1,15 @@
 import React from "react";
 import "../stylesheets/layout/_fill.scss";
 import "../stylesheets/layout/_collapsable-header.scss";
-import "../stylesheets/layout/_desing.scss";
+import Design from "./Design.js";
 
 function Form() {
   return (
     <div className="bg__container--design">
       <div className="main__pageContainer">
+        <Design />
+        {/* //agregar clase collapsed dentro de esta section// */}
         <section className="js-collapsable">
-          <h2
-            className="article article__title collapsable__header"
-            title="@@titleh2"
-          >
-            <span className="article__icon far fa-object-ungroup"></span>
-            {/* {@@title} {@@iconImg} */}
-            <span className="article__title">Diseña</span>
-            <span className="fas fa-chevron-up article__icon--collapsible collapsable__header--arrow"></span>
-          </h2>
-          {/* COLAPASABLE DISEÑA -- pendiente aclarar que hacer con el @@ cuando hagamos componentes */}
-          {/* <include
-                src="./collapsable-header.html"
-                title="Diseña"
-                iconImg="far fa-object-ungroup"
-                titleh2=""
-              ></include> */}
-
-          <div className="design collapsable__container">
-            <h2 className="design__title">Colores</h2>
-
-            <div className="design__colorcontainer">
-              <label className="design__squareLabel">
-                <input
-                  className="js-palette"
-                  type="radio"
-                  name="palette"
-                  value="0"
-                  checked
-                />
-                <ul className="palette-0">
-                  <li className="design__squareColor color-1"></li>
-                  <li className="design__squareColor color-2"></li>
-                  <li className="design__squareColor color-3"></li>
-                </ul>
-              </label>
-
-              <label className="design__squareLabel">
-                <input
-                  className="js-palette"
-                  type="radio"
-                  name="palette"
-                  value="1"
-                />
-                <ul className="palette-1">
-                  <li className="design__squareColor color-1"></li>
-                  <li className="design__squareColor color-2"></li>
-                  <li className="design__squareColor color-3"></li>
-                </ul>
-              </label>
-
-              <label className="design__squareLabel">
-                <input
-                  className="js-palette"
-                  type="radio"
-                  name="palette"
-                  value="2"
-                />
-                <ul className="palette-2">
-                  <li className="design__squareColor color-1"></li>
-                  <li className="design__squareColor color-2"></li>
-                  <li className="design__squareColor color-3"></li>
-                </ul>
-              </label>
-            </div>
-          </div>
-        </section>
-
-        <section className="js-collapsable collapsed">
           <h2
             className="article article__title collapsable__header"
             title="@@titleh2"
@@ -196,7 +130,6 @@ function Form() {
             </fieldset>
           </form>
         </section>
-
         <section className="js-collapsable collapsed">
           <h2
             className="article article__title collapsable__header"
