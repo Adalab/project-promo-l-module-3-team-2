c60b5106b7712c60b09aac804cc43e3e6ae954d2
Carmen Ramos <carmen.rama91@gmail.com>
2021-02-24 01:35:59 +0100

    add collapsable clicked and hidden

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stylesheets/layout/_collapsable-header.scss";
 
 function Collapsable(props) {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const openClassName = open ? "" : "close";
+
   return (
-    <div className="collapsable__container">
+    <div className={`collapsable__container ${openClassName}`}>
       <h2
         class="article article__title collapsable__header"
         title={props.title2}
+        onClick={handleClick}
       >
         <span class={`article__icon ${props.iconImg}`} />
         <span class="article__title">{props.title}</span>
         <span class="fas fa-chevron-up article__icon--collapsible collapsable__header--arrow"></span>
       </h2>
-      <div className="collapsable__content">{props.children}</div>
+      <div className={`collapsable-content ${openClassName}`}>
+        {props.children}
+      </div>
     </div>
   );
 }
