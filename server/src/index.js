71ac631ee53bb56b72db574fc1e4580daa86c473
TeresaMartinezFernandez <teresamf64@gmail.com>
2021-03-13 11:01:28 +0100

    Create server folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,21 @@
 const cors = require("cors");
 const express = require("express");
 // data
-const usersData = require("./data/users.json");
-console.log(usersData);
+// const usersData = require("./data/users.json");
+// console.log(usersData);
 // SERVER
 // config server
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 // init express aplication
 const serverPort = 3000;
 app.listen(serverPort, () => {
   console.log(`App listening at http://localhost:${serverPort}`);
+});
+
+app.post("/card", (req, res) => {
+  console.log(req.body);
+  res.json(["hola"]);
 });
--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,5 +1,5 @@
 const fetchAPI = (data) => {
-  const url = "https://localhost:3000.herokuapp.com/card";
+  const url = "http://localhost:3000/card";
 
   return fetch(url, {
     method: "POST",
@@ -15,48 +15,3 @@ const fetchAPI = (data) => {
 };
 
 export default fetchAPI;
-
-/*
-const shareButton = document.querySelector('.js-share-btn');
-const cardResult = document.querySelector('.js-shareclick');
-
-const twitterHiddenElement = document.querySelector('.js-twitter-share');
-
-let shareLink = '';
-
-function handleCreateBtn(ev) {
-  ev.preventDefault();
-
-  fetchAPI();
-
-  shareButton.classList.remove('share__button');
-  shareButton.classList.add('share__button--dis');
-  shareButton.disabled = true;
-  twitterHiddenElement.classList.remove('hidden-share');
-}
-
-shareButton.addEventListener('click', handleCreateBtn);
-
-function fetchAPI() {
-  const url = 'https://profileawesome.herokuapp.com/card';
-  const data = getUserData();
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log('Server response:', data);
-      if (data.success === true) {
-        data.cardURL;
-        shareLink = data.cardURL;
-        const linkElement = document.querySelector('.js-card-link');
-        linkElement.innerHTML = `<a href="${shareLink}" class="shareclick__text js-shareclick" target="_blank">${shareLink}</a>;`;
-      } else {
-        cardResult.innerHTML = data.error;
-      }
-    });
-}*/
