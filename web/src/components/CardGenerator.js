428fa5eb628a0ed3606e8e2fb3a7ff597ec8e4d3
saramartara <saramartinezaragon@gmail.com>
2021-03-14 20:41:32 +0100

    add defaultAvatar to CardGenerator function

--- web/src/components/CardGenerator.js
+++ web/src/components/CardGenerator.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Header from "./Header.js";
-import Main from "./Main.js";
-import Footer from "./Footer.js";
-import fetchAPI from "../services/Api";
-import ls from "../services/LocalStorage";
+import React, { useState, useEffect } from 'react';
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+import fetchAPI from '../services/Api';
+import ls from '../services/LocalStorage';
+import defaultAvatar from '../images/cardPhoto.png';
 
 function CardGenerator() {
-  const localStorageData = ls.get("data") || {};
+  const localStorageData = ls.get('data') || {};
   const [palettes, setPalettes] = useState(localStorageData.palettes || 0);
   const [name, setName] = useState(localStorageData.name);
   const [job, setJob] = useState(localStorageData.job);
@@ -14,11 +15,13 @@ function CardGenerator() {
   const [phone, setPhone] = useState(localStorageData.phone);
   const [linkedin, setLinkedin] = useState(localStorageData.linkedin);
   const [github, setGithub] = useState(localStorageData.github);
-  const [avatar, setAvatar] = useState(localStorageData.avatar);
+  const [avatar, setAvatar] = useState(
+    localStorageData.avatar || defaultAvatar
+  );
   const [serverData, setServerData] = useState({});
 
   useEffect(() => {
-    ls.set("data", {
+    ls.set('data', {
       palettes: palettes,
       name: name,
       job: job,
@@ -41,19 +44,19 @@ function CardGenerator() {
   // const [serverData, setServerData] = useState({});
 
   const handleInput = (inputId, inputValue) => {
-    if (inputId === "name") {
+    if (inputId === 'name') {
       setName(inputValue);
-    } else if (inputId === "job") {
+    } else if (inputId === 'job') {
       setJob(inputValue);
-    } else if (inputId === "email") {
+    } else if (inputId === 'email') {
       setEmail(inputValue);
-    } else if (inputId === "phone") {
+    } else if (inputId === 'phone') {
       setPhone(inputValue);
-    } else if (inputId === "linkedin") {
+    } else if (inputId === 'linkedin') {
       setLinkedin(inputValue);
-    } else if (inputId === "github") {
+    } else if (inputId === 'github') {
       setGithub(inputValue);
-    } else if (inputId === "palette") {
+    } else if (inputId === 'palette') {
       setPalettes(inputValue);
     }
   };
@@ -63,18 +66,18 @@ function CardGenerator() {
   }
 
   const handleReset = () => {
-    setName("");
-    setJob("");
-    setPhone("");
-    setEmail("");
-    setLinkedin("");
-    setGithub("");
-    setPalettes("0");
-    setAvatar("");
+    setName('');
+    setJob('');
+    setPhone('');
+    setEmail('');
+    setLinkedin('');
+    setGithub('');
+    setPalettes('0');
+    setAvatar('');
   };
 
   const handleShare = () => {
-    console.log("me han clicado");
+    console.log('me han clicado');
     const userData = {
       palette: parseInt(palettes) + 1,
       name: name,
