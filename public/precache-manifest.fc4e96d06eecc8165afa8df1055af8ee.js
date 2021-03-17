e6c06082ddc943055088352e935deae51403d095
TeresaMartinezFernandez <teresamf64@gmail.com>
2021-03-17 20:48:10 +0100

    run build

---
+++ public/precache-manifest.fc4e96d06eecc8165afa8df1055af8ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd9d4354a1571ffe8d01",
+    "url": "./static/css/main.5530d499.chunk.css"
+  },
+  {
+    "revision": "dd9d4354a1571ffe8d01",
+    "url": "./static/js/main.dd9d4354.chunk.js"
+  },
+  {
+    "revision": "8bfe72b5e98b219fa941",
+    "url": "./static/js/1.8bfe72b5.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b1eb7674fd8f0519392198b8c15a242c",
+    "url": "./static/media/cardPhoto.b1eb7674.png"
+  },
+  {
+    "revision": "af34b7d88247c79b3b542313163cac8a",
+    "url": "./static/media/title-creatures.af34b7d8.svg"
+  },
+  {
+    "revision": "8d6cecb6cbd29f6a230a4c7428b3c545",
+    "url": "./index.html"
+  }
+];
